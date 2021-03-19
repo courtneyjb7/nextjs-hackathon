38b2d0b834aad19af06838e825e342ada4382ec4
courtneyjb7 <72003830+courtneyjb7@users.noreply.github.com>
2021-03-19 13:52:53 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
+
+  const YourComponent = () => (
+    <Image
+      src="/images/profile.jpg" // Route of the image file
+      height={108} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+  )
   return (
     <div className="container">
       <Head>
@@ -9,10 +20,19 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+      <h1 className="title">
+        Jonathan and Courtney's{' '}
+        <Link href="/posts/first-post">
+          <a>website!</a>
+        </Link>
+      </h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={108} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+      
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
