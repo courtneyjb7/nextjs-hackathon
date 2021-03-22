9ac2ffe505a3c09ac8b4f66c10c7ec35f791bafc
courtneyjb7 <72003830+courtneyjb7@users.noreply.github.com>
2021-03-21 17:11:45 -0700

    update

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -25,6 +25,8 @@ export default function Post({ postData }) {
         {postData.id}
         <br />
         {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
     )
   }
